720171945ceb7227bf73a2b92593b02d469a7240
Doreen9 <doreen.dodoo@turntabl.io>
2019-11-20 15:02:07 +0000

    added tests

--- src/main/java/io/turntabl/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/io/turntabl/springbootdemo/SpringbootdemoApplication.java
@@ -1,10 +1,19 @@
 package io.turntabl.springbootdemo;
 
+import io.turntabl.springbootdemo.model.Maths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
+
+	@Bean
+	public Maths getMathFunctions(){
+		return new Maths();
+	}
+
 
 	public static void main(String[] args) {
 
